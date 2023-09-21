a5c2af31fe83b595d5be3f4be5f2ca05ede92044
subair <subairt10@gmail.com>
2023-09-21 10:04:58 +0530

    secondcommit

---
+++ palindrome_ababb.dart
@@ -0,0 +1,35 @@
+class Solution {
+  int removePalindromeSub(String s) {
+    if (s.isEmpty) {
+      return 0;
+    }
+  
+    if (_isPalindrome(s)) {
+      return 1;
+    }
+  
+    return 2;
+  }
+
+  bool _isPalindrome(String s) {
+    int left = 0;
+    int right = s.length - 1;
+  
+    while (left < right) {
+      if (s[left] != s[right]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+  
+    return true;
+  }
+}
+
+void main() {
+  Solution solution = Solution();
+  String inputString = "abb"; 
+  int result = solution.removePalindromeSub(inputString);
+  print("Result: $result");
+}
